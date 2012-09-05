1c4157edd7a9b44a9600f0210b5b06add0bd3490
Johannes J. Schmidt <schmidt@netzmerk.com>
2012-09-05 09:03:57 +0200

    define toolbar in html

--- bloke.js
+++ bloke.js
@@ -3,92 +3,24 @@
  * Author: Johannes J. Schmidt
  * (c) 2012 null2 GmbH
  * */
-
-;(function($, doc, win) {
-  var name = 'bloke',
-      selector = '[contenteditable]';
-
-  var formats = [
-    {
-      title: 'Heading',
-      command: 'formatBlock',
-      value: 'h2'
-    },
-    {
-      title: 'Paragraph',
-      command: 'formatBlock',
-      value: 'p'
-    },
-    {
-      title: 'Italic',
-      command: 'italic'
-    },
-    {
-      title: 'Bold',
-      command: 'bold'
-    },
-    {
-      title: 'Underline',
-      command: 'underline'
-    },
-    {
-      title: 'List',
-      command: 'insertUnorderedList'
-    },
-    {
-      title: 'Link',
-      command: 'createLink',
-      get: 'href',
-      ask: 'Please enter the URL. Use "mailto:me@example.com" to link to an e-mail address.',
-      default: 'http://'
-    }
-  ];
-
-  function buildToolbar() {
-    var toolbar = $('<menu>');
-
-    toolbar.addClass('bloke-toolbar');
-    toolbar.css('position', 'absolute');
-
-    formats.forEach(function(format) {
-      var button = $('<button>');
-
-      button.attr('title', format.title);
-
-      button.attr('data-command', format.command);
-      button.attr('data-value', format.value);
-      button.attr('data-get', format.get);
-      button.attr('data-ask', format.ask);
-      button.attr('data-default', format.default);
-
-      button.html(format.title);
-
-      toolbar.append(button);
-    });
-
-    return toolbar;
-  }
+(function($, doc, win) {
+  var selector = '[contenteditable]';
 
   function Bloke(el, opts) {
     this.$el      = $(el);
-    this.$el.data(name, this);
 
-    this.defaults = {};
-
-    var meta      = this.$el.data(name + '-opts');
-    this.opts     = $.extend(this.defaults, opts, meta);
-
-    var $toolbar = buildToolbar();
+    var $toolbar = this.$el.find('.bloke-toolbar');
 
     this.$el.on('focus', '[contenteditable]', function(e) {
-      $(e.target).before($toolbar);
+      $(e.target).before($toolbar.show());
     });
     this.$el.on('blur', '[contenteditable]', function(e) {
       var $el = $(this);
 
       // wait a bit
       setTimeout(function() {
-        if (!$el.is(':focus')) $toolbar.detach();
+        // still blured?
+        if (!$el.is(':focus')) $toolbar.hide();
       }, 500);
     });
     this.$el.on('click', '.bloke-toolbar', function(e) {
